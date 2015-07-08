9eea15d69b7d9c57b68917afbfec92d9f7cb80bf
johnb9682 <johnb9682@gmail.com>
2015-07-09 00:06:46 +0800

    source code

---
+++ app/src/main/java/com/johnb9682/instchat/client/Constants.java
@@ -0,0 +1,18 @@
+package com.johnb9682.instchat.client;
+
+/**
+ * @author appsrox.com
+ *
+ */
+public interface Constants {
+
+    /**
+     * Base URL of the Demo Server (such as http://my_host:8080/gcm-demo)
+     */
+	String SERVER_URL = "http://couplemsg.appspot.com";
+
+    /**
+     * Google API project id registered to use GCM.
+     */
+	String SENDER_ID = "79740512464";
+}
